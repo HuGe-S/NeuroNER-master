fbe4449caf3c4c3180e308f8be658a85f464ad63
Huyue Li <huyue@ualberta.ca>
2022-02-10 17:07:56 -0700

    possibility accum create

--- neuroner/train.py
+++ neuroner/train.py
@@ -7,118 +7,195 @@ import warnings
 import numpy as np
 import sklearn.metrics
 import tensorflow as tf
+
 # from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
 
 from neuroner.evaluate import remap_labels
 from neuroner import utils_tf
 from neuroner import utils_nlp
+from sklearn.preprocessing import minmax_scale
+from sklearn.preprocessing import normalize
+
 
 def train_step(sess, dataset, sequence_number, model, parameters):
     """
     Train.
     """
     # Perform one iteration
-    token_indices_sequence = dataset.token_indices['train'][sequence_number]
+    token_indices_sequence = dataset.token_indices["train"][sequence_number]
 
     for i, token_index in enumerate(token_indices_sequence):
-        if token_index in dataset.infrequent_token_indices and np.random.uniform() < 0.5:
+        if (
+            token_index in dataset.infrequent_token_indices
+            and np.random.uniform() < 0.5
+        ):
             token_indices_sequence[i] = dataset.UNK_TOKEN_INDEX
 
     feed_dict = {
-      model.input_token_indices: token_indices_sequence,
-      model.input_label_indices_vector: dataset.label_vector_indices['train'][sequence_number],
-      model.input_token_character_indices: dataset.character_indices_padded['train'][sequence_number],
-      model.input_token_lengths: dataset.token_lengths['train'][sequence_number],
-      model.input_label_indices_flat: dataset.label_indices['train'][sequence_number],
-      model.dropout_keep_prob: 1-parameters['dropout_rate']}
+        model.input_token_indices: token_indices_sequence,
+        model.input_label_indices_vector: dataset.label_vector_indices["train"][
+            sequence_number
+        ],
+        model.input_token_character_indices: dataset.character_indices_padded["train"][
+            sequence_number
+        ],
+        model.input_token_lengths: dataset.token_lengths["train"][sequence_number],
+        model.input_label_indices_flat: dataset.label_indices["train"][sequence_number],
+        model.dropout_keep_prob: 1 - parameters["dropout_rate"],
+    }
 
     _, _, loss, accuracy, transition_params_trained = sess.run(
-                    [model.train_op, model.global_step, model.loss, model.accuracy,
-                    model.transition_parameters],feed_dict)
+        [
+            model.train_op,
+            model.global_step,
+            model.loss,
+            model.accuracy,
+            model.transition_parameters,
+        ],
+        feed_dict,
+    )
 
     return transition_params_trained
 
-def prediction_step(sess, dataset, dataset_type, model, transition_params_trained,
-    stats_graph_folder, epoch_number, parameters, dataset_filepaths):
+
+def prediction_step(
+    sess,
+    dataset,
+    dataset_type,
+    model,
+    transition_params_trained,
+    stats_graph_folder,
+    epoch_number,
+    parameters,
+    dataset_filepaths,
+):
     """
     Predict.
     """
-    if dataset_type == 'deploy':
-        print('Predict labels for the {0} set'.format(dataset_type))
+    if dataset_type == "deploy":
+        print("Predict labels for the {0} set".format(dataset_type))
     else:
-        print('Evaluate model on the {0} set'.format(dataset_type))
+        print("Evaluate model on the {0} set".format(dataset_type))
 
     all_predictions = []
     all_y_true = []
-    output_filepath = os.path.join(stats_graph_folder, '{1:03d}_{0}.txt'.format(dataset_type,
-        epoch_number))
-    output_file = codecs.open(output_filepath, 'w', 'UTF-8')
-    original_conll_file = codecs.open(dataset_filepaths[dataset_type], 'r', 'UTF-8')
+    output_filepath = os.path.join(
+        stats_graph_folder, "{1:03d}_{0}.txt".format(dataset_type, epoch_number)
+    )
+    output_filepath_score = os.path.join(
+        stats_graph_folder, "{1:03d}_{0}_score.txt".format(dataset_type, epoch_number)
+    )
+    output_file = codecs.open(output_filepath, "w", "ISO-8859-1")
+    output_file_score = codecs.open(output_filepath_score, "w", "ISO-8859-1")
+    original_conll_file = codecs.open(
+        dataset_filepaths[dataset_type], "r", "ISO-8859-1"
+    )
 
     for i in range(len(dataset.token_indices[dataset_type])):
         feed_dict = {
-          model.input_token_indices: dataset.token_indices[dataset_type][i],
-          model.input_token_character_indices: dataset.character_indices_padded[dataset_type][i],
-          model.input_token_lengths: dataset.token_lengths[dataset_type][i],
-          model.input_label_indices_vector: dataset.label_vector_indices[dataset_type][i],
-          model.dropout_keep_prob: 1.
+            model.input_token_indices: dataset.token_indices[dataset_type][i],
+            model.input_token_character_indices: dataset.character_indices_padded[
+                dataset_type
+            ][i],
+            model.input_token_lengths: dataset.token_lengths[dataset_type][i],
+            model.input_label_indices_vector: dataset.label_vector_indices[
+                dataset_type
+            ][i],
+            model.dropout_keep_prob: 1.0,
         }
 
-        unary_scores, predictions = sess.run([model.unary_scores,
-            model.predictions], feed_dict)
+        unary_scores, predictions = sess.run(
+            [model.unary_scores, model.predictions], feed_dict
+        )
+        # print(unary_scores)
+        if parameters["use_crf"]:
+            predictions, _ = tf.contrib.crf.viterbi_decode(
+                unary_scores, transition_params_trained
+            )
 
-        if parameters['use_crf']:
-            predictions, _ = tf.contrib.crf.viterbi_decode(unary_scores,
-                transition_params_trained)
             predictions = predictions[1:-1]
         else:
             predictions = predictions.tolist()
 
-        assert(len(predictions) == len(dataset.tokens[dataset_type][i]))
-
-        output_string = ''
-        prediction_labels = [dataset.index_to_label[prediction] for prediction in predictions]
+        assert len(predictions) == len(dataset.tokens[dataset_type][i])
+        # print(predictions)
+        output_string = ""
+        prediction_labels = [
+            dataset.index_to_label[prediction] for prediction in predictions
+        ]
+        # print(dataset.index_to_label)
         unary_score_list = unary_scores.tolist()[1:-1]
-
+        normalize_unary_scores = normalize(
+            np.exp(unary_scores[1:-1]), norm="l1", axis=1
+        )
+        # print(normalize_unary_scores)
         gold_labels = dataset.labels[dataset_type][i]
 
-        if parameters['tagging_format'] == 'bioes':
+        if parameters["tagging_format"] == "bioes":
             prediction_labels = utils_nlp.bioes_to_bio(prediction_labels)
             gold_labels = utils_nlp.bioes_to_bio(gold_labels)
 
-        for prediction, token, gold_label, scores in zip(prediction_labels,
-            dataset.tokens[dataset_type][i], gold_labels, unary_score_list):
+        for prediction, token, gold_label, scores in zip(
+            prediction_labels,
+            dataset.tokens[dataset_type][i],
+            gold_labels,
+            normalize_unary_scores,
+        ):
 
             while True:
                 line = original_conll_file.readline()
-                split_line = line.strip().split(' ')
+                # print(line)
+                split_line = line.strip().split(" ")
 
-                if '-DOCSTART-' in split_line[0] or len(split_line) == 0 \
-                or len(split_line[0]) == 0:
+                if (
+                    "-DOCSTART-" in split_line[0]
+                    or len(split_line) == 0
+                    or len(split_line[0]) == 0
+                ):
                     continue
                 else:
                     token_original = split_line[0]
 
-                    if parameters['tagging_format'] == 'bioes':
+                    if parameters["tagging_format"] == "bioes":
                         split_line.pop()
 
                     gold_label_original = split_line[-1]
 
-                    assert(token == token_original and gold_label == gold_label_original)
+                    # assert token == token_original and gold_label == gold_label_original
                     break
 
             split_line.append(prediction)
             try:
-                if parameters['output_scores']:
-                    # space separated scores
-                    scores = ' '.join([str(i) for i in scores])
-                    split_line.append('{}'.format(scores))
+                if parameters["output_scores"]:
+                    # # space separated scores
+                    # N = 5
+
+                    # # Indices of N largest elements in list
+                    # # using sorted() + lambda + list slicing
+                    # res = sorted(range(len(scores)), key=lambda sub: scores[sub])[-N:]
+                    # topscores = []
+                    # # print(dataset.index_to_label[i])
+                    # # print(topscores)
+                    # for i in res:
+                    #     topscores.append(
+                    #         dataset.index_to_label[i]
+                    #         + " "
+                    #         + str("{:.1%}".format(scores[i]))
+                    #     )
+
+                    # scores = ",".join([i for i in topscores[::-1]])
+                    # split_line.append("," + "{}".format(scores))
+                    for i in range(len(scores)):
+                        scores[i] = "{}".format(scores[i])
+                    scores = " ".join([str(i) for i in scores])
+                    split_line.append("{}".format(scores))
+
             except KeyError:
                 pass
 
-            output_string += ' '.join(split_line) + '\n'
+            output_string += " ".join(split_line) + "\n"
 
-        output_file.write(output_string+'\n')
+        output_file.write(output_string + "\n")
 
         all_predictions.extend(predictions)
         all_y_true.extend(dataset.label_indices[dataset_type][i])
@@ -126,38 +203,63 @@ def prediction_step(sess, dataset, dataset_type, model, transition_params_traine
     output_file.close()
     original_conll_file.close()
 
-    if dataset_type != 'deploy':
+    if dataset_type != "deploy":
 
-        if parameters['main_evaluation_mode'] == 'conll':
+        if parameters["main_evaluation_mode"] == "conll":
 
             # run perl evaluation script in python package
             # conll_evaluation_script = os.path.join('.', 'conlleval')
-            package_name = 'neuroner'
-            root_dir = os.path.dirname(pkg_resources.resource_filename(package_name,
-                '__init__.py'))
-            conll_evaluation_script = os.path.join(root_dir, 'conlleval')
+            package_name = "neuroner"
+            root_dir = os.path.dirname(
+                pkg_resources.resource_filename(package_name, "__init__.py")
+            )
+            conll_evaluation_script = os.path.join(root_dir, "conlleval")
 
-            conll_output_filepath = '{0}_conll_evaluation.txt'.format(output_filepath)
-            shell_command = 'perl {0} < {1} > {2}'.format(conll_evaluation_script,
-                output_filepath, conll_output_filepath)
+            conll_output_filepath = "{0}_conll_evaluation.txt".format(output_filepath)
+            shell_command = "perl {0} < {1} > {2}".format(
+                conll_evaluation_script, output_filepath, conll_output_filepath
+            )
             os.system(shell_command)
 
-            with open(conll_output_filepath, 'r') as f:
+            with open(conll_output_filepath, "r") as f:
                 classification_report = f.read()
                 print(classification_report)
 
         else:
-            new_y_pred, new_y_true, new_label_indices, new_label_names, _, _ = remap_labels(all_predictions,
-                all_y_true, dataset, parameters['main_evaluation_mode'])
+            (
+                new_y_pred,
+                new_y_true,
+                new_label_indices,
+                new_label_names,
+                _,
+                _,
+            ) = remap_labels(
+                all_predictions, all_y_true, dataset, parameters["main_evaluation_mode"]
+            )
 
-            print(sklearn.metrics.classification_report(new_y_true, new_y_pred, 
-                digits=4, labels=new_label_indices, target_names=new_label_names))
+            print(
+                sklearn.metrics.classification_report(
+                    new_y_true,
+                    new_y_pred,
+                    digits=4,
+                    labels=new_label_indices,
+                    target_names=new_label_names,
+                )
+            )
 
     return all_predictions, all_y_true, output_filepath
 
 
-def predict_labels(sess, model, transition_params_trained, parameters, dataset,
-    epoch_number, stats_graph_folder, dataset_filepaths):
+def predict_labels(
+    sess,
+    model,
+    transition_params_trained,
+    parameters,
+    dataset,
+    epoch_number,
+    stats_graph_folder,
+    dataset_filepaths,
+):
     """
     Predict labels using trained model
     """
@@ -165,13 +267,27 @@ def predict_labels(sess, model, transition_params_trained, parameters, dataset,
     y_true = {}
     output_filepaths = {}
 
-    for dataset_type in ['train', 'valid', 'test', 'deploy']:
+    for dataset_type in ["train", "valid", "test", "deploy"]:
         if dataset_type not in dataset_filepaths.keys():
             continue
 
-        prediction_output = prediction_step(sess, dataset, dataset_type, model,
-            transition_params_trained, stats_graph_folder, epoch_number,
-            parameters, dataset_filepaths)
-        y_pred[dataset_type], y_true[dataset_type], output_filepaths[dataset_type] = prediction_output
+        prediction_output = prediction_step(
+            sess,
+            dataset,
+            dataset_type,
+            model,
+            transition_params_trained,
+            stats_graph_folder,
+            epoch_number,
+            parameters,
+            dataset_filepaths,
+        )
+
+        (
+            y_pred[dataset_type],
+            y_true[dataset_type],
+            output_filepaths[dataset_type],
+        ) = prediction_output
+        # print(y_pred, y_true)
 
     return y_pred, y_true, output_filepaths
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # import glob
@@ -16,60 +16,51 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 #     print(directory)
 
 setup(
-    name='pyneuroner',
-
+    name="pyneuroner",
+    author="huyue",
+    author_email="lihuyue05@gmail.com",
     # Versions should comply with PEP440. For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.8',
-
-    description='NeuroNER',
+    version="1.0.8",
+    description="NeuroNER",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-
+    long_description_content_type="text/markdown",
     # The project's main homepage.
-    url='https://github.com/Franck-Dernoncourt/NeuroNER',
-
+    url="https://github.com/Franck-Dernoncourt/NeuroNER",
     # Author details
     # author='',
     # author_email='',
-
     # Choose your license
     # license='MIT',
-
     # What does your project relate to?
-    keywords='Named-entity recognition using neural networks',
-
+    keywords="Named-entity recognition using neural networks",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # packages=find_packages(exclude=['contrib', 'docs', 'tests','env',
     #     'output']),
-    packages=['neuroner'],
-
+    packages=["neuroner"],
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
     # py_modules=[''],
-
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'matplotlib>=3.0.2',
-        'networkx>=2.2',
-        'pycorenlp>=0.3.0',
-        'scikit-learn>=0.20.2',
-        'scipy>=1.2.0',
-        'spacy>=2.0.18',
-        ],
-
-    # allow user to select flavour of TensorFlow 
+        "matplotlib>=3.0.2",
+        "networkx>=2.2",
+        "pycorenlp>=0.3.0",
+        "scikit-learn>=0.20.2",
+        "scipy>=1.2.0",
+        "spacy>=2.0.18",
+    ],
+    # allow user to select flavour of TensorFlow
     # https://github.com/tensorflow/tensorflow/issues/7166
     extras_require={
         "cpu": ["tensorflow>=1.12.0"],
         "gpu": ["tensorflow-gpu>=1.0.0"],
     },
-
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
@@ -78,31 +69,25 @@ setup(
     #     'dev': ['check-manifest'],
     #     'test': ['coverage'],
     # },
-
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     zip_safe=False,
     # package_dir={'neuroner': 'neuroner'},
-    include_package_data = True,
-    package_data={'data': ['data/**'], 
-        'trained_models': ['trained_models/**']
-    },
-
+    include_package_data=True,
+    package_data={"data": ["data/**"], "trained_models": ["trained_models/**"]},
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     # data_files=[('my_data', ['data/data_file'])],
     # data_files=[('neuroner', ['conlleval'])],
-
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'console_scripts': [
-            'neuroner = neuroner.__main__:main',
+        "console_scripts": [
+            "neuroner = neuroner.__main__:main",
         ],
     },
-
 )
